15bea253888158795309215df522c6ddd4e10f16
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-05-17 22:36:22 +0200

    Add Icons to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
+import InboxIcon from "@material-ui/icons/Inbox";
+import PersonIcon from "@material-ui/icons/Person";
 import Button from "../UI/Button/Button";
 import NavigationLinks from "../Navigation/NavigationLinks/NavigationLinks";
 
@@ -13,15 +14,17 @@ const Header = (props) => {
     <header className={"header"}>
       <div className={"header__left"}>
         <Link to={"/"} className={"header__logo"}>
-          Inbox MY STORAGE
+          <InboxIcon />
+          MY STORAGE
         </Link>
         <div className={"header__line"} />
         <NavigationLinks userRole={userRole} />
       </div>
       <div className={"header__right"}>
-        <div
-          className={"header__username"}
-        >{`${user.firstName} ${user.lastName}`}</div>
+        <div className={"header__username"}>
+          <PersonIcon height={16} />
+          {`${user.firstName} ${user.lastName}`}
+        </div>
         <Button clicked={onLogout}>LOGOUT</Button>
       </div>
     </header>
