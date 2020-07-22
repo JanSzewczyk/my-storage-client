084c1a6d5b25a3803419b8051c9e2990bd7ed357
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-07-22 21:49:26 +0200

    Remove craco

--- craco.config.js
+++ craco.config.js
@@ -1,43 +1,26 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-  // rootDir: path.resolve(__dirname, "./"),
-
-  webpack: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@assets": path.resolve(__dirname, "src/assets/"),
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@config": path.resolve(__dirname, "src/shared/config/"),
-      "@containers": path.resolve(__dirname, "src/containers/"),
-      "@hoc": path.resolve(__dirname, "src/hoc/"),
-      "@hooks": path.resolve(__dirname, "src/hooks/"),
-      "@shared": path.resolve(__dirname, "src/shared/"),
-      "@store": path.resolve(__dirname, "src/store/"),
-      "@UI": path.resolve(__dirname, "src/components/UI/"),
-      "@utils": path.resolve(__dirname, "src/shared/utils/"),
-    },
-  },
-  jest: {
-    configure: {
-      globals: {
-        CONFIG: true,
-      },
-      // moduleFileExtensions: [
-      //   "web.js",
-      //   "js",
-      //   "web.ts",
-      //   "ts",
-      //   "web.tsx",
-      //   "tsx",
-      //   "json",
-      //   "web.jsx",
-      //   "jsx",
-      //   "node",
-      // ],
-      moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-      },
-    },
-  },
-};
+// module.exports = {
+//   webpack: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src/"),
+//       "@assets": path.resolve(__dirname, "src/assets/"),
+//       "@components": path.resolve(__dirname, "src/components/"),
+//       "@config": path.resolve(__dirname, "src/shared/config/"),
+//       "@containers": path.resolve(__dirname, "src/containers/"),
+//       "@hoc": path.resolve(__dirname, "src/hoc/"),
+//       "@hooks": path.resolve(__dirname, "src/hooks/"),
+//       "@shared": path.resolve(__dirname, "src/shared/"),
+//       "@store": path.resolve(__dirname, "src/store/"),
+//       "@UI": path.resolve(__dirname, "src/components/UI/"),
+//       "@utils": path.resolve(__dirname, "src/shared/utils/"),
+//     },
+//   },
+//   jest: {
+//     configure: {
+//       moduleNameMapper: {
+//         "^@/(.*)$": "<rootDir>/src/$1",
+//       },
+//     },
+//   },
+// };
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-import { createJestConfig } from "@craco/craco";
-
-import cracoConfig from "./craco.config.js";
-
-const jestConfig = createJestConfig(cracoConfig);
-
-export default jestConfig;
