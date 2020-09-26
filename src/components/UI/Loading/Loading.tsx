b2b2326c80bac1db7842dfcf14a736d0b90e7b95
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-09-27 00:07:11 +0200

    Migrate Loading -> ts

--- src/components/UI/Loading/Loading.tsx
+++ src/components/UI/Loading/Loading.tsx
@@ -3,7 +3,9 @@ import Loader from "react-loader-spinner";
 
 import "./Loading.scss";
 
-const Loading = (props) => (
+interface LoadingProps {}
+
+const Loading: React.FC<LoadingProps> = (props) => (
   <div className={"loading"}>
     <Loader type="BallTriangle" color={"#0082f0"} width={120} height={80} />
   </div>
