5af54a3ed3c5925b8d8536816aca25dc40610f9b
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-09-27 00:05:44 +0200

    Migrate Input -> ts

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { InputConfig, InputType } from "./types";
+import { FixMeLater } from "../../../shared/types/common/FixMeLater";
+
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+
+import "./Input.scss";
+
+interface InputProps {
+  labelClass?: string;
+  refInput: FixMeLater;
+  config: InputConfig;
+  inputClass?: string;
+  label?: string;
+  errorClass?: string;
+  hasError?: boolean;
+  errorMessage?: string;
+  inputType?: InputType;
+}
+
+const Input: React.FC<InputProps> = ({
+  labelClass,
+  refInput,
+  config,
+  inputClass,
+  label,
+  errorClass,
+  hasError,
+  errorMessage,
+  inputType,
+}) => {
+  let labelClasses: string[] = ["label"];
+  let inputClasses: string[] = ["input"];
+  let errorClasses: string[] = ["error"];
+
+  inputType && labelClasses.push(`label--${inputType}`);
+  inputType && inputClasses.push(`input--${inputType}`);
+
+  hasError && inputClasses.push("input--invalid");
+
+  labelClass && labelClasses.push(labelClass);
+  inputClass && inputClasses.push(inputClass);
+  errorClass && errorClasses.push(errorClass);
+
+  return (
+    <Aux>
+      {label && (
+        <label htmlFor={config.name} className={labelClasses.join(" ")}>
+          {label}
+        </label>
+      )}
+      <input
+        id={config.name}
+        className={inputClasses.join(" ")}
+        ref={refInput}
+        {...config}
+      />
+      {hasError && errorMessage && (
+        <div className={errorClasses.join(" ")}>{errorMessage}</div>
+      )}
+    </Aux>
+  );
+};
+
+export default Input;
