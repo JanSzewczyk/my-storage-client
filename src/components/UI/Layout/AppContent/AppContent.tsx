3b7b0cabfc1427bb7ae0dfa5ae3663af627cf45f
JanSzewczyk <jan.szewczyk1997@gmail.com>
2021-03-02 21:09:26 +0100

    Added color

--- src/components/UI/Layout/AppContent/AppContent.tsx
+++ src/components/UI/Layout/AppContent/AppContent.tsx
@@ -4,11 +4,9 @@ import PropsWithChildren from "../../../../shared/types/props/PropsWithChildren"
 
 import "./AppContent.scss";
 
-// TODO flex wrap options / column
 interface AppContentProps extends PropsWithChildren<ReactNode> {}
 
 const AppContent: React.FC<AppContentProps> = ({ children }) => {
-  
   return <div className={"app-content"}>{children}</div>;
 };
 
