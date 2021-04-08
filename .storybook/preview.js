9003a860f45a024e06af0735c19aee50d8c4f299
JanSzewczyk <jan.szewczyk1997@gmail.com>
2021-04-08 22:05:19 +0200

    fix input styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "../src/index.scss";
 import { addDecorator } from "@storybook/react";
 
 const styles = {
   margin: "8px",
   display: "flex",
-  wight: "100%",
+  wight: "100%"
   // justifyContent: "center",
 };
 
